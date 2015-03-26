07dd312df74bab225dc3b9a645f3aa71dd5a5a28
Ryan Storberg <ryan@storberg.net>
2015-03-26 02:15:17 -0400

    Initialize

---
+++ javascripts/general.js
@@ -0,0 +1,11 @@
+var myFirebaseRef = new Firebase("https://idea-room.firebaseio.com/");
+
+function Card() {
+	// this.id = Stuff
+}
+
+$(document).on('ready', function() {
+	$('button').on('click', function() {
+		// Stuff
+	})
+})
