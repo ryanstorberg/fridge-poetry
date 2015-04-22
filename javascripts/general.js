07f6e5dea931344ca5237bd18d94149b62609402
Ryan Storberg <ryan@storberg.net>
2015-04-22 13:02:55 -0400

    Refactor JavaScript; fix word duplications; separate word list into separate file

--- javascripts/general.js
+++ javascripts/general.js
@@ -1,17 +1,20 @@
-var word_set = ["&","boil","for","is","&","boy","forest","is","a","breast","frantic","it","a","but","friend","it","a","but","from","it","a","but","from","juice","a","but","garden","lake","a","butt","girl","language","about","by","go","languid","above","by","goddess","lather","ache","can","gorgeous","lazy","ad","chant","gown","less","after","chocolate","hair","let","all","cool","has","lick","am","could","have","lie","am","crush","have","life","an","cry","he","light","an","d","he","like","and","day","head","like","and","death","heave","like","and","delirious","her","live","and","diamond","her","love","apparatus","did","here","luscious","are","do","him","lust","are","do","his","ly","arm","dream","his","ly","as","dress","honey","ly","as","drive","hot","ly","as","drool","how","mad","as","drunk","I","man","ask","eat","I","me","at","ed","I","me","at","ed","I","me","at","ed","if","mean","away","ed","in","meat","bare","egg","in","men","be","elaborate","in","milk","beat","enormous","ing","mist","beauty","er","ing","moan","bed","es","ing","moon","beneath","est","ing","mother","bitter","fast","ing","music","black","feet","ing","must","blood","fiddle","is","my","blow","finger","is","my","blue","fluff","is","my","need","s","the","y","never","s","the","y","no","s","their","yet","no","s","there","you","not","s","these","you","not","sad","they","you","of","said","those","you","of","sausage","though","of","say","thousand","of","scream","through","on","sea","time","on","see","tiny","one","shadow","to","or","she","to","our","she","to","over","shine","together","pant","ship","tongue","peach","shot","trudge","petal","show","TV","picture","sing","ugly","pink","sit","up","play","skin","urge","please","sky","us","pole","sleep","use","pound","smear","want","puppy","smell","want","purple","smooth","was","put","so","watch","r","soar","water","r","some","wax","rain","sordid","we","raw","spray","we","recall","spring","were","red","still","what","repulsive","stop","when","rip","storm","whisper","rock","suit","who","rose","summer","why","run","sun","will","rust","sweat","wind","s","sweet","with","s","swim","with","s","symphony","woman","s","the","worship","s","the","y","s","the","y"]
 var users = new Firebase("https://fridge-poetry.firebaseio.com/users");
 var words = new Firebase("https://fridge-poetry.firebaseio.com/words");
 var currentUser = users.push({'user':'active'});
-currentUser.onDisconnect().remove();
+var currentElement;
+
+$('button').on('click', function() {
+		writeWord();
+})
 
 words.on('child_added', function(data, prevChildName) {
 	readWord(data);
-	makeDraggable();
 })
 
 words.on('child_changed', function(data, prevChildName) {
-	readPosition(data);
-	makeDraggable();
+	if(data.key() != currentElement) {
+		readPosition(data);
+	}
 })
 
 words.on('child_removed', function(data) {
@@ -19,10 +22,45 @@ words.on('child_removed', function(data) {
 })
 
 users.on('value', function(data) {
-	updateUsers(data);
+	readUsers(data);
 })
 
-function updateUsers(data) {
+currentUser.onDisconnect().remove();
+
+function makeDraggable(word) {
+	$('#' + word.key()).draggable({
+		start: function() {
+			currentElement = $(this).attr('id');
+			writePosition(this);
+			$(this).css('z-index', '2');
+			$('button').css('background-color', 'rgb(175, 125, 125)');
+			$('button img').attr('src', 'images/minus.png');
+		},
+		drag: function() {
+			writePosition(this);
+		},
+		stop: function() {
+			currentElement = null;
+			writePosition(this);
+			$(this).css('z-index', '0');
+			$('button').css('background-color', 'rgb(125, 175, 125)');
+			$('button img').attr('src', 'images/plus.png');
+
+			wordTop = $(this).position().top + ($(this).height() / 2);
+			wordLeft = $(this).position().left + ($(this).width() / 2);
+			buttonStartTop = $('button').position().top;
+			buttonEndTop = $('button').position().top + 50;
+			buttonStartLeft = $('button').position().left
+			buttonEndLeft = $('button').position().left + 50;
+
+			if((wordTop > buttonStartTop && wordTop < buttonEndTop) && (wordLeft > buttonStartLeft && wordLeft < buttonEndLeft)) {
+				words.child($(this).attr('id')).remove();
+			}
+		}
+	});
+}
+
+function readUsers(data) {
 	var userCount = data.numChildren();
 	if(userCount > 2) {
 		$('#user-count p').text('You\'re sharing this screen with ' + (userCount - 1) + ' other people');
@@ -33,78 +71,27 @@ function updateUsers(data) {
 	}
 }
 
-function addHandlers() {
-	$('button').on('click', function() {
-		getWord();
-	})
-}
-
-
-function getWord () {
-	var word = word_set[Math.floor(Math.random() * word_set.length)];
-	writeWord(word);
-}
-
-function randomVerical() {
-	return Math.round(Math.random() * ($(window).height() - 100));
-}
-
-function randomHorizontal() {
-	return Math.round(Math.random() * ($(window).width() - 100));
-}
-
-function makeDraggable() {
-	$('.word').draggable({
-		start: function() {
-			writePosition(this);
-			$(this).css('z-index', '2');
-			$('button').css('background-color', 'rgb(175, 125, 125)');
-			$('button img').attr('src', 'images/minus.png');
-		},
-		drag: function() {
-			writePosition(this);
-		},
-		stop: function() {
-			writePosition(this);
-			$(this).css('z-index', '0');
-			$('button').css('background-color', 'rgb(125, 175, 125)');
-			$('button img').attr('src', 'images/plus.png');
-
-			wordTop = $(this).position().top + ($(this).height() / 2);
-			wordLeft = $(this).position().left + ($(this).width() / 2);
-			buttonStartTop = $('button').position().top;
-			buttonEndTop = $('button').position().top + 50
-			buttonStartLeft = $('button').position().left
-			buttonEndLeft = $('button').position().left + 50
-
-			if((wordTop > buttonStartTop && wordTop < buttonEndTop) && (wordLeft > buttonStartLeft && wordLeft < buttonEndLeft)) {
-				words.child($(this).attr('id')).remove();
-			}
-		}
-	});
-}
-
-function readWords(words) {
-	words.forEach(function(word) {
-		var top = word.child('position/top').val();
-		var left = word.child('position/left').val();
-		$('body').prepend('<p id=' + word.key() + ' class=\'word ui-widget-content\' style=\'top: ' + top + 'px; left: ' + left + 'px\'>' + word.val()['term'] + '</p>');
-	})
-}
-
-function writeWord(word) {
+function writeWord() {
+	var word = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
 	var wordObject = {};
 	wordObject['term'] = word;
 	wordObject['position'] = {};
-	wordObject['position']['top'] = randomVerical();
-	wordObject['position']['left'] = randomHorizontal();
+	wordObject['position']['top'] = Math.round(Math.random() * ($(window).height() - 100));
+	wordObject['position']['left'] = Math.round(Math.random() * ($(window).width() - 100));
 	words.push(wordObject);
 }
 
 function readWord(word) {
 	var top = word.child('position/top').val();
 	var left = word.child('position/left').val();
-	$('body').prepend('<p id=' + word.key() + ' class=\'word ui-widget-content\' style=\'top: ' + top + 'px; left: ' + left + 'px\'>' + word.val()['term'] + '</p>');
+	$('body').prepend(
+		"<p id='" + word.key() + "'" +
+		"class='word ui-widget-content'" +
+		"style='top:" + top + "px; left:" + left + "px'>" +
+		word.child('term').val() +
+		"</p>"
+	);
+	makeDraggable(word);
 }
 
 function writePosition(word) {
@@ -124,9 +111,6 @@ function readPosition(word) {
 
 $(document).on('ready', function() {
 	users.once('value', function(data) {
-		updateUsers(data);
+		readUsers(data);
 	})
-	words.once('value', function(data) {
-		readWords(data);
-	}, addHandlers())
 })
